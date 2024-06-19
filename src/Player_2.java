4a2960dd905f62051c3ffb755bad8f07336d48af
Jonthz <jnzambra@fiec.edu.espol.ec>
2024-06-18 23:06:00 -0500

    liskov principle sugestion for a better code

--- src/Player_2.java
+++ src/Player_2.java
@@ -1,54 +1,61 @@
 public class Player_2 extends Player_1{
-    private int health;
-    private Boolean armour;
+    private boolean armour;
 
-
-    public Player_2(String name, String weapon, int health, Boolean armour) {
+    public Player_2(String name, String weapon, int health, boolean armour) {
         super(name, weapon, health);
-        this.health = health;
         this.armour = armour;
-
     }
 
     @Override
     public void Hit_By_Gun1() {
-        if (armour){
-            this.health-=10;
-            if(this.health<=0) {
-                this.health = 0;
+        // Changes for solve a liskov principle violation in the code
+
+        // To mantain the liskov principle we need to change the code in the following way
+
+        super.Hit_By_Gun1(); // This will call the method from the parent class and will reduce the health by 20
+
+        if (getHealth() > 0) { 
+            int extraDamage;
+            if (armour) {
+                extraDamage = -10; 
+            } else {
+                extraDamage = 20; 
             }
-            System.out.println("Armour is on. got hit by gun 1 . health is reduced by 10 and new health is :"+ this.health);
-        }
-        if (!armour) {
-            this.health -= 40;
-            if (this.health <= 0) {
-                this.health = 0;
+
+            setHealth(getHealth() - extraDamage);
+
+            if (getHealth() <= 0) {
+                setHealth(0);
+                System.out.println(getName() + " is dead");
+            } else {
+                if (armour) {
+                    System.out.println("Armour is on. Additional damage applied. New health is: " + getHealth());
+                } else {
+                    System.out.println("Armour is off. Additional damage applied. New health is: " + getHealth());
+                }
             }
-            System.out.println("Armour is off. got hit by gun 1 . health is reduced by 40 and new health is :" + this.health);
-        }
-        if (this.health==0){
-            System.out.println(getName()+" is dead");
         }
     }
 
     @Override
     public void Hit_By_Gun2() {
-        if (armour){
-            this.health-=30;
-            if(this.health<=0) {
-                this.health = 0;
-            }
-            System.out.println("Armour is on. got hit by gun 2 . health is reduced by 30 and new health is :"+ this.health);
+        int damage;
+        if (armour) {
+            damage = 30;
+        } else {
+            damage = 60;
         }
-        if (!armour) {
-            this.health -= 60;
-            if (this.health <= 0) {
-                this.health = 0;
+    
+        setHealth(getHealth() - damage); // Use the super class method to have consistent behavior
+        if (getHealth() <= 0) {
+            setHealth(0);
+            System.out.println(getName() + " is dead");
+        } else {
+            if (armour) {
+                System.out.println("Armour is on. Hit by gun 2. Health is reduced by " + damage + " and new health is: " + getHealth());
+            } else {
+                System.out.println("Armour is off. Hit by gun 2. Health is reduced by " + damage + " and new health is: " + getHealth());
             }
-            System.out.println("Armour is off. got hit by gun 2 . health is reduced by 60 and new health is :" + this.health);
-        }
-        if (this.health==0){
-            System.out.println(getName()+" is dead");
         }
 
     }
